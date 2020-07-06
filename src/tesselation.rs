a7d120addca20495f8f8948824b67a2d30cfa5a8
Vanille-N <neven.villani@gmail.com>
2020-07-06 10:11:36 +0200

    refactor common pattern

--- src/tesselation.rs
+++ src/tesselation.rs
@@ -3,12 +3,47 @@ use crate::shape::*;
 use std::collections::HashSet;
 use svg::node::element::Path;
 
+macro_rules! set {
+    { $( $elem:expr ),* } => {
+        {
+            let mut set = HashSet::new();
+            $( set.insert($elem); )*
+            set
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 pub struct Frame {
     pub x: usize,
     pub y: usize,
     pub w: usize,
     pub h: usize,
+}
+
+fn periodic_grid_tiling<F>(f: &Frame, gen: F, idir: Pos, jdir: Pos) -> Vec<Path>
+where
+    F: Fn(Pos) -> Vec<Path>,
+{
+    let mut v = Vec::new();
+    let center = f.center();
+    let mut set = set![center];
+    let mut stk = vec![center];
+    while let Some(pos) = stk.pop() {
+        if f.is_inside(pos) {
+            for item in gen(pos) {
+                v.push(item);
+            }
+            for &(i, j) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let p = pos + idir * i + jdir * j;
+                if !set.contains(&p) {
+                    set.insert(p);
+                    stk.push(p);
+                }
+            }
+        }
+    }
+    v
 }
 
 impl Frame {
@@ -31,104 +66,48 @@ impl Frame {
 }
 
 pub fn tile_hexagons(f: &Frame, size: f64, rot: i32) -> Vec<Path> {
-    let mut v = Vec::new();
-    let center = f.center();
     let idir = polar(radians(rot - 30), (size * 2.) * radians(30).cos());
     let jdir = polar(radians(rot + 30), (size * 2.) * radians(30).cos());
-    let mut set = HashSet::new();
-    let mut stk = Vec::new();
-    stk.push(center);
-    set.insert(center);
-    while !stk.is_empty() {
-        let pos = stk.pop().unwrap();
-        if f.is_inside(pos) {
-            v.push(pos);
-            for &(i, j) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let p = pos + idir * i + jdir * j;
-                if !set.contains(&p) {
-                    set.insert(p);
-                    stk.push(p);
-                }
-            }
-        }
-    }
     let m = Movable::hexagon(size, rot);
-    v.into_iter().map(|p| m.render(p)).collect::<Vec<_>>()
+    periodic_grid_tiling(f, |p| vec![m.render(p)], idir, jdir)
 }
 
 pub fn tile_triangles(f: &Frame, size: f64, rot: i32) -> Vec<Path> {
-    let mut v = Vec::new();
-    let center = f.center();
     let idir = polar(radians(rot - 30), (size * 2.) * radians(30).cos());
     let jdir = polar(radians(rot + 30), (size * 2.) * radians(30).cos());
-    let adjust = polar(radians(rot + 60), size * radians(30).sin());
-    let mut set = HashSet::new();
-    let mut stk = Vec::new();
-    stk.push(center);
-    set.insert(center);
-    while !stk.is_empty() {
-        let pos = stk.pop().unwrap();
-        if f.is_inside(pos) {
-            v.push((pos, false));
-            v.push((pos + idir * 0.5 + adjust, true));
-            for &(i, j) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let p = pos + idir * i + jdir * j;
-                if !set.contains(&p) {
-                    set.insert(p);
-                    stk.push(p);
-                }
-            }
-        }
-    }
-    let m1 = Movable::triangle(size, rot);
-    let m2 = Movable::triangle(size, rot + 60);
-    v.into_iter()
-        .map(|(p, b)| (if b { &m1 } else { &m2 }).render(p))
-        .collect::<Vec<_>>()
+    let adjust = polar(radians(rot + 60), size * radians(30).sin()) + idir * 0.5;
+    let m1 = Movable::triangle(size, rot + 60);
+    let m2 = Movable::triangle(size, rot);
+    periodic_grid_tiling(f, |p| vec![m1.render(p), m2.render(p + adjust)], idir, jdir)
 }
 
 pub fn tile_hybrid_hexagons_triangles(f: &Frame, size: f64, rot: i32) -> Vec<Path> {
-    let mut v = Vec::new();
-    let center = f.center();
     let idir = polar(radians(rot), size * 2.);
     let jdir = polar(radians(rot + 60), size * 2.);
     let adjust = polar(radians(rot + 30), size / radians(30).cos());
-    let mut set = HashSet::new();
-    let mut stk = Vec::new();
-    stk.push(center);
-    set.insert(center);
-    while !stk.is_empty() {
-        let pos = stk.pop().unwrap();
-        if f.is_inside(pos) {
-            v.push((pos, 0));
-            v.push((pos + adjust, 1));
-            v.push((pos - adjust, 2));
-            for &(i, j) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let p = pos + idir * i + jdir * j;
-                if !set.contains(&p) {
-                    set.insert(p);
-                    stk.push(p);
-                }
-            }
-        }
-    }
     let m = [
         Movable::hexagon(size, rot),
         Movable::triangle(size * radians(30).sin(), rot + 30),
         Movable::triangle(size * radians(30).sin(), rot + 90),
     ];
-    v.into_iter()
-        .map(|(p, i)| m[i].render(p))
-        .collect::<Vec<_>>()
+    periodic_grid_tiling(
+        f,
+        |p| {
+            vec![
+                m[0].render(p),
+                m[1].render(p + adjust),
+                m[2].render(p - adjust),
+            ]
+        },
+        idir,
+        jdir,
+    )
 }
 
 pub fn tile_hybrid_squares_triangles(f: &Frame, size: f64, rot: i32) -> Vec<Path> {
-    let mut v = Vec::new();
-    let center = f.center();
-    let mut set = HashSet::new();
-    let mut stk = Vec::new();
-    stk.push(center);
-    set.insert(center);
+    let a = size / 2_f64.sqrt();
+    let b = a * radians(30).tan();
+    let c = a / radians(30).cos();
     //
     //  +---------------+,
     //  |            ,' |,'-,
@@ -140,39 +119,10 @@ pub fn tile_hybrid_squares_triangles(f: &Frame, size: f64, rot: i32) -> Vec<Path
     //  |               | ,-'
     //  +---------------+'
     //
-    let a = size / 2_f64.sqrt();
-    let b = a * radians(30).tan();
-    let c = a / radians(30).cos();
-    let idir = polar(radians(rot), c + a*2. + 2.*b) + polar(radians(rot + 60), c + a*2. + 2.*b);
-    let jdir = polar(radians(rot), c + a*2. + 2.*b) + polar(radians(rot - 60), c + a*2. + 2.*b);
-    while !stk.is_empty() {
-        let pos = stk.pop().unwrap();
-        if f.is_inside(pos) {
-            for i in 0..6 {
-                v.push((
-                    pos + polar(radians(rot + i * 60), c),
-                    3 + (i as usize % 2),
-                ));
-                v.push((
-                    pos + polar(radians(rot + i * 60), c + b + a),
-                    i as usize % 3,
-                ));
-                v.push((
-                    pos + polar(radians(rot + i * 60 + 30), 2. * a + c),
-                    5 + (i as usize % 2),
-                ));
-            }
-            v.push((pos + polar(radians(rot), c + 2. * b + 2. * a), 4));
-            v.push((pos - polar(radians(rot), c + 2. * b + 2. * a), 3));
-            for &(i, j) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let p = pos + idir * i + jdir * j;
-                if !set.contains(&p) {
-                    set.insert(p);
-                    stk.push(p);
-                }
-            }
-        }
-    }
+    let idir =
+        polar(radians(rot), c + a * 2. + 2. * b) + polar(radians(rot + 60), c + a * 2. + 2. * b);
+    let jdir =
+        polar(radians(rot), c + a * 2. + 2. * b) + polar(radians(rot - 60), c + a * 2. + 2. * b);
     let m = [
         Movable::square(size, rot),
         Movable::square(size, rot + 60),
@@ -182,7 +132,24 @@ pub fn tile_hybrid_squares_triangles(f: &Frame, size: f64, rot: i32) -> Vec<Path
         Movable::triangle(c, rot + 90),
         Movable::triangle(c, rot + 30),
     ];
-    v.into_iter()
-        .map(|(p, i)| m[i].render(p))
-        .collect::<Vec<_>>()
+    periodic_grid_tiling(
+        f,
+        |p| {
+            let mut v = vec![
+                m[4].render(p + polar(radians(rot), c + 2. * b + 2. * a)),
+                m[3].render(p - polar(radians(rot), c + 2. * b + 2. * a)),
+            ];
+            for i in 0..6 {
+                v.push(m[3 + (i as usize % 2)].render(p + polar(radians(rot + i * 60), c)));
+                v.push(m[i as usize % 3].render(p + polar(radians(rot + i * 60), c + b + a)));
+                v.push(
+                    m[5 + (i as usize % 2)]
+                        .render(p + polar(radians(rot + i * 60 + 30), 2. * a + c)),
+                );
+            }
+            v
+        },
+        idir,
+        jdir,
+    )
 }
