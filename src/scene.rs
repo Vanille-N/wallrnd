94edc12de158c509b5f6312d7bdf5e9c4f62f176
Vanille-N <neven.villani@gmail.com>
2020-07-06 10:51:35 +0200

    implementation for Disc

--- src/scene.rs
+++ src/scene.rs
@@ -31,3 +31,19 @@ pub trait Contains {
 pub fn create_items() -> Vec<Box<dyn Contains>> {
     vec![Box::new(Disc { center: Pos(500., 400.), radius: 100., color: Color(255, 0, 0) })]
 }
+
+struct Disc {
+    center: Pos,
+    radius: f64,
+    color: Color,
+}
+
+impl Contains for Disc {
+    fn contains(&self, p: Pos) -> Option<Color> {
+        if (self.center - p).dot_self() < self.radius.powi(2) {
+            Some(self.color)
+        } else {
+            None
+        }
+    }
+}
