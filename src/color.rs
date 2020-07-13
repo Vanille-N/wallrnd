6a8967373ec6d150065bc89511a27d977244bc50
Vanille-N <neven.villani@gmail.com>
2020-07-13 15:03:02 +0200

    comments in color.rs

--- src/color.rs
+++ src/color.rs
@@ -4,6 +4,7 @@ use rand::{rngs::ThreadRng, Rng};
 pub struct Color(pub i32, pub i32, pub i32);
 
 impl Color {
+    /// Ensure that all RGB values are within [[1; 100]]
     fn validate(mut self) -> Self {
         self.0 = self.0.min(255).max(0);
         self.1 = self.1.min(255).max(0);
@@ -11,6 +12,7 @@ impl Color {
         self
     }
 
+    /// Random noise
     pub fn variate(mut self, rng: &mut ThreadRng, amount: i32) -> Self {
         self.0 += rng.gen_range(-amount, amount);
         self.1 += rng.gen_range(-amount, amount);
@@ -18,6 +20,7 @@ impl Color {
         self
     }
 
+    /// Weighted mix with other color
     pub fn meanpoint(mut self, th: Self, weight: i32) -> Self {
         self.0 = (self.0 * weight + th.0 * (100 - weight)) / 100;
         self.1 = (self.1 * weight + th.1 * (100 - weight)) / 100;
@@ -25,6 +28,7 @@ impl Color {
         self
     }
 
+    /// Generate color
     pub fn random(rng: &mut ThreadRng) -> Self {
         Self(
             rng.gen_range(0, 255),
@@ -34,6 +38,7 @@ impl Color {
     }
 }
 
+/// SVG color format: `rgb(<r>,<g>,<b>)`
 impl ToString for Color {
     fn to_string(&self) -> String {
         let c = self.validate();
