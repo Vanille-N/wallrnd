6bacefc8014726227f31033e02e35cee4930274a
Vanille-N <neven.villani@gmail.com>
2020-07-23 10:55:05 +0200

    added progress prints

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,17 @@ fn main() {
     let verbose = args.verbose;
 
     if args.init != "" {
+        if verbose.prog {
+            println!("Initializing configuration file");
+        }
         make_config_file(&args.init[..]);
         std::process::exit(0);
     }
 
     // Get local time and convert to app-specific format: HHMM
+    if verbose.prog {
+        println!("Reading time");
+    }
     let time = args.time.unwrap_or_else(|| {
         let now = Local::now();
         let h = now.hour();
@@ -35,7 +41,13 @@ fn main() {
     let dest = args.image;
     let fname = args.config;
 
+    if verbose.prog {
+        println!("Creating random number generator");
+    }
     let mut rng = rand::thread_rng();
+    if verbose.prog {
+        println!("Attempting to open configuration file");
+    }
     let cfg_file = File::open(fname);
     let mut cfg_contents = String::new();
     if let Ok(mut f) = cfg_file {
@@ -47,14 +59,23 @@ fn main() {
     } else if verbose.warn {
         println!("Settings file not found");
     }
+    if verbose.prog {
+        println!("Choosing random settings according to configuration");
+    }
     let cfg = MetaConfig::from_string(cfg_contents, verbose).pick_cfg(&mut rng, time, verbose);
 
+    if verbose.prog {
+        println!("Building scene");
+    }
     let scene = Scene::new(&cfg, &mut rng);
     let stroke = cfg.line_color;
     let stroke_width = cfg.line_width;
     let stroke_like_fill = stroke_width < 0.0001;
 
     // Generate document
+    if verbose.prog {
+        println!("Creating tiling");
+    }
     let mut document = Document::new(cfg.frame);
     for (pos, elem) in cfg.make_tiling(&mut rng) {
         let fill = scene.color(pos, &mut rng);
@@ -65,12 +86,18 @@ fn main() {
         );
     }
 
+    if verbose.prog {
+        println!("Writing image to file");
+    }
     document.save(dest).unwrap_or_else(|_| {
         if verbose.warn {
             println!("No valid destination specified");
-            std::process::exit(1);
         }
+        std::process::exit(1);
     });
+    if verbose.prog {
+        println!("Process exited successfully");
+    }
 }
 
 #[derive(Default)]
