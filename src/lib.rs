5fbd51dd115d251342de864b201700e6c760e499
Vanille-N <neven.villani@gmail.com>
2020-07-23 10:45:30 +0200

    add verbosity controller

--- src/lib.rs
+++ src/lib.rs
@@ -17,4 +17,35 @@ pub mod prelude {
     pub use color::Color;
     pub use frame::Frame;
     pub use pos::{radians, Pos};
+    pub use super::Verbosity;
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct Verbosity {
+    pub info: bool,
+    pub warn: bool,
+    pub prog: bool,
+    pub details: bool,
+}
+
+impl Verbosity {
+    pub fn from(s: &str) -> Self {
+        let mut v = Verbosity::default();
+        for option in s.chars() {
+            match option {
+                'A' => {
+                    v.info = true;
+                    v.warn = true;
+                    v.prog = true;
+                    v.details = true;
+                }
+                'I' => v.info = true,
+                'W' => v.warn = true,
+                'P' => v.prog = true,
+                'D' => v.details = true,
+                c => println!("Unknown verbosity option '{}', use one or more of 'IWPDA'", c),
+            }
+        }
+        v
+    }
 }
