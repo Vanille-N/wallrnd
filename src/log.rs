d5634b6c873c428e3f75b7f8d75ddb6d2feae05e
Vanille-N <neven.villani@gmail.com>
2020-07-24 08:55:47 +0200

    Basic Display impls

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,22 @@
 use crate::prelude::*;
 use crate::scene::*;
+use std::fmt;
 pub struct Logger<'a> {
     pub frame: Frame,
     pub bg: &'a ColorItem,
     pub objects: &'a [Box<dyn Contains>],
+}
+
+impl fmt::Display for Frame {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {} {}", self.x, self.y, self.w, self.h)
+    }
+}
+
+impl fmt::Display for ColorItem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {} ", self.shade.0, self.shade.1, self.shade.2)?;
+        write!(f, "{} {} {} ", self.theme.0, self.theme.1, self.theme.2)?;
+        write!(f, "{} {}", self.deviation, self.weight)
+    }
 }
