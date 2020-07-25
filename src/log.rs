7fe1b6de32fa9b6efae5ad02933099beb58e866e
Vanille-N <neven.villani@gmail.com>
2020-07-25 10:32:53 +0200

    added # to indicate termination

--- src/log.rs
+++ src/log.rs
@@ -11,7 +11,7 @@ pub struct Logger<'a> {
 
 impl fmt::Display for Frame {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {} {} {}", self.x, self.y, self.w, self.h)
+        write!(f, "{} {} {} {} #", self.x, self.y, self.w, self.h)
     }
 }
 
@@ -19,21 +19,21 @@ impl fmt::Display for ColorItem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} {} {} ", self.shade.0, self.shade.1, self.shade.2)?;
         write!(f, "{} {} {} ", self.theme.0, self.theme.1, self.theme.2)?;
-        write!(f, "{} {}", self.deviation, self.weight)
+        write!(f, "{} {} #", self.deviation, self.weight)
     }
 }
 
 impl fmt::Display for Disc {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Disc {} {} {} ", self.center.0, self.center.1, self.radius)?;
-        write!(f, "{}", self.color)
+        write!(f, "{} #", self.color)
     }
 }
 
 impl fmt::Display for HalfPlane {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "HalfPlane {} {} {} {} ", self.limit.0, self.limit.1, self.reference.0, self.reference.1)?;
-        write!(f, "{}", self.color)
+        write!(f, "{} #", self.color)
     }
 }
 
@@ -42,28 +42,28 @@ impl fmt::Display for Triangle {
         write!(f, "Triangle {} {} ", self.a.0, self.a.1)?;
         write!(f, "{} {} ", self.b.0, self.b.1)?;
         write!(f, "{} {} ", self.c.0, self.c.1)?;
-        write!(f, "{}", self.color)
+        write!(f, "{} #", self.color)
     }
 }
 
 impl fmt::Display for Spiral {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Spiral {} {} {} ", self.center.0, self.center.1, self.width)?;
-        write!(f, "{}", self.color)
+        write!(f, "{} #", self.color)
     }
 }
 
 impl fmt::Display for Stripe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Stripe {} {} {} {} ", self.limit.0, self.limit.1, self.reference.0, self.reference.1)?;
-        write!(f, "{}", self.color)
+        write!(f, "{} #", self.color)
     }
 }
 
 impl fmt::Display for Wave {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Wave {} {} {} {} ", self.limit.0, self.limit.1, self.reference.0, self.reference.1)?;
-        write!(f, "{}", self.color)
+        write!(f, "{} #", self.color)
     }
 }
 
@@ -75,7 +75,7 @@ impl<'a> fmt::Display for Logger<'a> {
         for o in self.objects {
             write!(f, "{} ", o)?;
         }
-        Ok(())
+        write!(f, "#")
     }
 }
 
