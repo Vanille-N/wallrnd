d39f2b9808e1fd9ae8b2d66c319a27800fbf2d6c
Vanille-N <neven.villani@gmail.com>
2020-08-15 16:30:51 +0200

    attempts ?

--- src/tesselate.rs
+++ src/tesselate.rs
@@ -203,12 +203,12 @@ pub fn pentagons_type1(f: &Frame, size: f64, rot: isize) -> Vec<(Pos, Path)> {
 pub fn pentagons_type2(f: &Frame, size: f64, rot: isize) -> Vec<(Pos, Path)> {
     let alpha = 140;
     let beta = 70;
-    let gamma = 100;
+    let gamma = 110;
     let delta = 180 - beta;
-    let epsilon = 100;
+    let epsilon = 110;
     let sizes = [size, size*1.2, size];
     let angles = [alpha, beta, gamma, delta, epsilon];
-    let rangles = [gamma, beta, alpha, epsilon, delta];
+    let rangles = [delta, gamma, beta, alpha, epsilon];
     let mv = [
         Pentagon { sizes, rot, angles }.to_movable(),
         Pentagon { sizes, rot: rot + 180, angles }.to_movable(),
