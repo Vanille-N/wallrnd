e97e8d29a7f3d1b592a1a26efc9a730f58f2c58e
Vanille-N <neven.villani@gmail.com>
2020-08-25 10:52:00 +0200

    Hah ! I'm on to something.

--- src/tesselate.rs
+++ src/tesselate.rs
@@ -212,8 +212,8 @@ pub fn pentagons_type2(f: &Frame, size: f64, rot: isize) -> Vec<(Pos, Path)> {
     let mv = [
         Pentagon { sizes, rot, angles }.to_movable(),
         Pentagon { sizes, rot: rot + 180, angles }.to_movable(),
-        Pentagon { sizes, rot: rot + alpha as isize, angles: rangles }.to_movable(),
-        Pentagon { sizes, rot: rot + alpha as isize , angles: rangles }.to_movable(),
+        Pentagon { sizes, rot: rot + 180, angles: rangles }.to_movable(),
+        Pentagon { sizes, rot: rot, angles: rangles }.to_movable(),
     ];
     let idir = Pos::polar(0, size*100.);
     let jdir = Pos::polar(90, size*100.);
@@ -223,6 +223,7 @@ pub fn pentagons_type2(f: &Frame, size: f64, rot: isize) -> Vec<(Pos, Path)> {
             vec![
                 mv[0].render(pos),
                 mv[1].render(pos + mv[0].vertex(2) + mv[3].side(4) - mv[1].vertex(1)),
+                mv[2].render(pos + mv[0].vertex(0) - mv[2].vertex(1)),
                 mv[3].render(pos + mv[0].vertex(2) - mv[3].vertex(4)),
             ]
         }
