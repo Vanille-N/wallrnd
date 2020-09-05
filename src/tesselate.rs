b1dc2c398b3089dae29a0a368c9790a90e9b3a3a
Vanille-N <neven.villani@gmail.com>
2020-09-05 07:34:16 +0200

    primitive pattern completed !

--- src/tesselate.rs
+++ src/tesselate.rs
@@ -289,6 +289,10 @@ pub fn pentagons_type3(f: &Frame, size: f64, rot: isize) -> Vec<(Pos, Path)> {
                 mv[1].render(pos - mv[5].vertex(2) + mv[5].vertex(4) - mv[1].vertex(4)),
                 mv[4].render(pos - mv[0].vertex(2) + mv[0].vertex(4) - mv[4].vertex(4)),
                 mv[5].render(pos - mv[1].vertex(2) + mv[1].vertex(4) - mv[5].vertex(4)),
+                mv[0].render(pos - mv[2].vertex(2) + mv[2].vertex(4) - mv[0].vertex(4)),
+                mv[1].render(pos - mv[3].vertex(2) + mv[3].vertex(4) - mv[1].vertex(4)),
+                mv[2].render(pos - mv[4].vertex(2) + mv[4].vertex(4) - mv[2].vertex(4)),
+                mv[3].render(pos - mv[5].vertex(2) + mv[5].vertex(4) - mv[3].vertex(4)),
             ]
         },
         idir,
